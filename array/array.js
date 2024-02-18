f111413e15754f9808b8bd31f35c4561b1299cad
SuryaRajendr <73171944+SuryaRajendr@users.noreply.github.com>
2024-02-18 16:51:34 +0000

    array added

---
+++ array/array.js
@@ -0,0 +1,14 @@
+// Question 1: How do you create an empty array in JavaScript?
+
+// let arr = []
+// let arr2 = new array()
+
+// Question 2: How do you access the first and last elements of an array?
+
+const fruits = ['apple', 'banana', 'orange', 'mango'];
+
+// remove 'orange'
+
+fruits.splice(2, 1);
+
+console.log(fruits); // ['apple', 'banana', 'mango']
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+const array = require('./array/array.js')
 console.log("Hello World");
+
