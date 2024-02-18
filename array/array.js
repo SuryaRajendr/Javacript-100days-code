61478cf302a3ab992456fa061bad3f5c830390e3
SuryaRajendr <73171944+SuryaRajendr@users.noreply.github.com>
2024-02-18 16:56:37 +0000

    added

--- array/array.js
+++ array/array.js
@@ -5,10 +5,16 @@
 
 // Question 2: How do you access the first and last elements of an array?
 
-const fruits = ['apple', 'banana', 'orange', 'mango'];
+const fruits = ["apple", "banana", "orange", "mango"];
 
 // remove 'orange'
 
 fruits.splice(2, 1);
 
 console.log(fruits); // ['apple', 'banana', 'mango']
+
+let arr = [1, 2, 3];
+
+arr.splice(2, 1);
+
+console.log(arr); // [1, 2]
